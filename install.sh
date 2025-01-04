1c653815b587cae11f789b1b3c8d5959118c17ae
Hilal <46250773+hilalsaygin@users.noreply.github.com>
2025-01-04 15:36:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 
 echo "Starting the environment setup..."
 python -m venv sv-env
-echo "Activating the virtual environment..."
 source sv-env/bin/activate
 
 echo "Installing dependencies..."
 pip install numpy torch torchvision torchaudio
 pip install -r requirements.txt
+echo "Setup complete!"
